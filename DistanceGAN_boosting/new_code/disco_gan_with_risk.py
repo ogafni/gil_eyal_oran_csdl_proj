f6567f1ae38f4c662564a73e74773164a49cef78
Oran <oran.gafni@gmail.com>
2018-01-06 21:26:02 +0200

    added support for one-sample training

--- DistanceGAN_boosting/new_code/disco_gan_with_risk.py
+++ DistanceGAN_boosting/new_code/disco_gan_with_risk.py
@@ -102,13 +102,80 @@ class DiscoGANRisk(DiscoGAN):
         correlation_loss_BA_2 = - self.correlation_criterion(BA_2, to_no_grad_var(BA_1))
         return correlation_loss_AB_2, correlation_loss_BA_2
 
+    def _calc_loss_one(self, generator_A, generator_B, discriminator_A, discriminator_B, A, B, one_sample_A, one_sample_B, rate):
+        one_sample_index = self.args.one_sample_index
+        generator_A.zero_grad()
+        generator_B.zero_grad()
+        discriminator_A.zero_grad()
+        discriminator_B.zero_grad()
+
+        AB = generator_B(A)
+        BA = generator_A(B)
+        ABA = generator_A(AB)
+        BAB = generator_B(BA)
+
+        one_sample_AB_full = generator_B(one_sample_A)
+        one_sample_BA_full = generator_A(one_sample_B)
+        one_sample_AB = one_sample_AB_full[one_sample_index]
+        one_sample_BA = one_sample_BA_full[one_sample_index]
+
+        one_sample_ABA = generator_A(one_sample_AB_full)[one_sample_index]
+        one_sample_BAB = generator_B(one_sample_BA_full)[one_sample_index]
+
+        # Reconstruction Loss
+        recon_loss_A = self.recon_criterion(ABA, A)
+        recon_loss_B = self.recon_criterion(BAB, B)
+        one_sample_recon_loss_A = self.recon_criterion(one_sample_ABA, one_sample_A[one_sample_index])
+        one_sample_recon_loss_B = self.recon_criterion(one_sample_BAB, one_sample_B[one_sample_index])
+
+        # Real/Fake GAN Loss (A)
+        A_dis_real, A_feats_real = discriminator_A(A)
+        A_dis_fake, A_feats_fake = discriminator_A(BA)
+        dis_loss_A, gen_loss_A = get_gan_loss(A_dis_real, A_dis_fake, self.gan_criterion, self.cuda)
+        fm_loss_A = get_fm_loss(A_feats_real, A_feats_fake, self.feat_criterion)
+
+        # Real/Fake GAN Loss (B)
+        B_dis_real, B_feats_real = discriminator_B(B)
+        B_dis_fake, B_feats_fake = discriminator_B(AB)
+        dis_loss_B, gen_loss_B = get_gan_loss(B_dis_real, B_dis_fake, self.gan_criterion, self.cuda)
+        fm_loss_B = get_fm_loss(B_feats_real, B_feats_fake, self.feat_criterion)
+
+        # Additional for one-sample mode (A)
+        A_dis_real, A_feats_real = discriminator_A(one_sample_A[one_sample_index: one_sample_index + 1])
+        A_dis_fake, A_feats_fake = discriminator_A(one_sample_BA_full[one_sample_index: one_sample_index + 1])
+        one_sample_dis_loss_A, one_sample_gen_loss_A = get_gan_loss(A_dis_real, A_dis_fake)
+        one_sample_fm_loss_A = get_fm_loss(A_feats_real, A_feats_fake)
+        gen_loss_A += one_sample_gen_loss_A
+        fm_loss_A += one_sample_fm_loss_A
+        recon_loss_A += one_sample_recon_loss_A
+
+        # Additional for one-sample mode (A)
+        B_dis_real, B_feats_real = discriminator_B(one_sample_B[one_sample_index: one_sample_index + 1])
+        B_dis_fake, B_feats_fake = discriminator_B(one_sample_AB_full[one_sample_index: one_sample_index + 1])
+        one_sample_dis_loss_B, one_sample_gen_loss_B = get_gan_loss(B_dis_real, B_dis_fake)
+        one_sample_fm_loss_B = get_fm_loss(B_feats_real, B_feats_fake)
+        gen_loss_B += one_sample_gen_loss_B
+        fm_loss_B += one_sample_fm_loss_B
+        recon_loss_B += one_sample_recon_loss_B
+
+        # Total Loss
+        gen_loss_A_total = (gen_loss_B * 0.1 + fm_loss_B * 0.9) * (1. - rate) + recon_loss_A * rate
+        gen_loss_B_total = (gen_loss_A * 0.1 + fm_loss_A * 0.9) * (1. - rate) + recon_loss_B * rate
+
+        return dis_loss_A, dis_loss_B, gen_loss_A_total, gen_loss_B_total, recon_loss_A, recon_loss_B
+
     def train(self, data_A, data_B, data_A_val, data_B_val, b_weights=None):
         n_batches = math.ceil((len(data_A)) / self.args.batch_size)
         print('%d batches per epoch' % n_batches)
         self.iters = 0
         a_dataloader, b_dataloader = get_data_loaders(data_A, data_B, self.args.batch_size, b_weights)
 
+        if self.args.one_sample_train:
+            aligned_path, _ = get_batch_data(data_A_val, data_A_val, 0, len(data_A_val))
+            one_sample_A, one_sample_B = read_images(aligned_path, aligned_path, self.args.image_size, self.cuda)
+
         for epoch in range(self.args.epoch_size):
+
             widgets = ['epoch #%d|' % epoch, Percentage(), Bar(), ETA()]
             pbar = ProgressBar(maxval=n_batches, widgets=widgets)
             pbar.start()
@@ -120,30 +187,39 @@ class DiscoGANRisk(DiscoGAN):
                 A, B = read_images(A_paths, B_paths, self.args.image_size, self.cuda)
 
                 # calculate losses
+                correlation_rate = self.args.default_correlation_rate
                 if self.iters < self.args.gan_curriculum:
+                    if not self.args.one_sample_train:
+                        correlation_rate = self.args.starting_correlation_rate
                     rate = self.args.starting_rate
-                    correlation_rate = self.args.starting_correlation_rate
                 else:
                     rate = self.args.default_rate
-                    correlation_rate = self.args.default_correlation_rate
 
-                self.dis_loss_A, self.dis_loss_B, self.gen_loss_A, self.gen_loss_B, self.recon_loss_A, self.recon_loss_B = self._calc_loss(
-                    self.generator_A, self.generator_B, self.discriminator_A, self.discriminator_B, A, B, rate)
-
-                self.dis_loss_A_2, self.dis_loss_B_2, self.gen_loss_A_2, self.gen_loss_B_2, self.recon_loss_A_2, self.recon_loss_B_2 = self._calc_loss(
-                    self.generator_A_G2, self.generator_B_G2, self.discriminator_A_G2, self.discriminator_B_G2, A, B,
-                    rate)
-
-                # correlation loss
-                correlation_loss_AB, correlation_loss_BA = self._calc_corr_loss(A, B)
+                # Calculate G1 for both cases
+                self.dis_loss_A, self.dis_loss_B, self.gen_loss_A, self.gen_loss_B, self.recon_loss_A, self.recon_loss_B = \
+                    self._calc_loss(self.generator_A, self.generator_B, self.discriminator_A, self.discriminator_B, A, B, rate)
+                # Calculate G2 for one-sample mode
+                if self.args.one_sample_train:
+                    self.dis_loss_A_2, self.dis_loss_B_2, self.gen_loss_A_2, self.gen_loss_B_2, self.recon_loss_A_2, self.recon_loss_B_2 = \
+                        self._calc_loss_one(self.generator_A_G2, self.generator_B_G2, self.discriminator_A_G2,
+                                            self.discriminator_B_G2, A, B, one_sample_A, one_sample_B, rate)
+                    correlation_loss_AB, correlation_loss_BA = self._calc_corr_loss(
+                        one_sample_A[self.args.one_sample_index], one_sample_B[self.args.one_sample_index])
+                # Calculate G2 for all-samples case
+                else:
+                    self.dis_loss_A_2, self.dis_loss_B_2, self.gen_loss_A_2, self.gen_loss_B_2, self.recon_loss_A_2, self.recon_loss_B_2 = \
+                        self._calc_loss(self.generator_A_G2, self.generator_B_G2, self.discriminator_A_G2,
+                                        self.discriminator_B_G2, A, B, rate)
+                    correlation_loss_AB, correlation_loss_BA = self._calc_corr_loss(A, B)
 
                 self.gen_loss_A_2 += correlation_loss_AB * correlation_rate
                 self.gen_loss_B_2 += correlation_loss_BA * correlation_rate
 
-                self.gen_loss_1 = self.gen_loss_A + self.gen_loss_B
-                self.dis_loss_1 = self.dis_loss_A + self.dis_loss_B
-                self.gen_loss_2 = self.gen_loss_A_2 + self.gen_loss_B_2
-                self.dis_loss_2 = self.dis_loss_A_2 + self.dis_loss_B_2
+                if self.args.model_arch == 'discogan':
+                    self.gen_loss_1 = self.gen_loss_A + self.gen_loss_B
+                    self.dis_loss_1 = self.dis_loss_A + self.dis_loss_B
+                    self.gen_loss_2 = self.gen_loss_A_2 + self.gen_loss_B_2
+                    self.dis_loss_2 = self.dis_loss_A_2 + self.dis_loss_B_2
 
                 # optimize
                 if self.iters % self.args.update_interval == 0:
