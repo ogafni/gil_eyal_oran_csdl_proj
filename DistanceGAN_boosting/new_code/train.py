aebb32d5bca3b3f955147771e1b956fecd977563
Eyal <shulmaneyal@gmail.com>
2018-01-21 09:44:59 +0200

    clean Gil's paths

--- DistanceGAN_boosting/new_code/train.py
+++ DistanceGAN_boosting/new_code/train.py
@@ -5,9 +5,6 @@ from new_code.disco_gan_model import DiscoGAN
 
 if __name__ == "__main__":
     options = Options()
-    options.model_save_interval = 100
-    options.model_path = '/home/gil/MEGA/temp/'
-    options.result_path = '/home/gil/dl_wolf_git/gil_eyal_oran_csdl_proj/DistanceGAN_boosting/saved_runs/'
     model = DiscoGAN(options)
     data_a, data_b = dataset.get_edges2shoes()
     data_a_val, data_b_val = dataset.get_edges2shoes(test=True, number_of_samples=options.batch_size)
