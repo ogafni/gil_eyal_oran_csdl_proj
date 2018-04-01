18e776598f4ceb9aacdc837d2a15201b59d563a6
gil <gilsharo@gmail.com>
2018-04-01 08:50:55 +0300

    -

--- DistanceGAN_boosting/new_code/train.py
+++ DistanceGAN_boosting/new_code/train.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__": # These settings are used since G1 fixed training is not implemented yet
     options = Options(task_name='clustering_debug', dataset='edges2shoes', model_path='/home/gil/MEGA/saved_models',
-                      result_path='/home/gil/dl_wolf_git/gil_eyal_oran_csdl_proj/DistanceGAN_boosting/results',)
+                      result_path='/home/gil/dl_wolf_git/gil_eyal_oran_csdl_proj/DistanceGAN_boosting/results',epoch_size=78*7)
     disco_gan = DiscoGAN(options)
     data_a, data_b = dataset.get_edges2shoes(test=False, cluster_file='/home/gil/MEGA/saved_models/referece/edges2shoes/kmenas.pkl',cluster_idx=0,is_cluster_A=False)
     data_a_val, data_b_val = dataset.get_edges2shoes(test=True, cluster_file='/home/gil/MEGA/saved_models/referece/edges2shoes/kmenas.pkl',cluster_idx=0,is_cluster_A=False)
